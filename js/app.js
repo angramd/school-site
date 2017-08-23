99085ce03d474fe25f90b6051355077f7cb7234a
Douglas Angram <angramd@1000-t02kfm23.local>
2017-08-23 17:06:04 -0500

    Updates to the Site

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-$(document).foundation()
+$(document).ready(function(){
+  var downArr = $("#downArr");
+
+  downArr.animate({"opacity": "1", "margin-top": "1%"}, 3000,
+	function()
+	{
+		downArr.addClass("animated css3-notification");
+	});
+});
