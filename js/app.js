d96f36f2d0ce3b9c9e9d542c6d4dae2e415de1a4
Douglas Angram <angramd@1000-t02kfm23.local>
2017-08-24 10:23:14 -0500

    More Updates to Sites

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   var downArr = $("#downArr");
-
-  downArr.animate({"opacity": "1", "margin-top": "1%"}, 3000,
+  downArr.css({"margin": "0 auto", "opacity": "0", "top": "0%"})
+  downArr.animate({"opacity": "1", "top": "1%"}, 3000,
 	function()
 	{
 		downArr.addClass("animated css3-notification");
