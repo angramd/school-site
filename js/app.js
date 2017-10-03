a6ae79b29dd3706b17a1fd6607aafce7ef08749a
Douglas Angram <angramd@1000-41QFJ1G9.local>
2017-10-03 17:04:57 -0500

    Styling for Firefox

--- js/app.js
+++ js/app.js
@@ -67,7 +67,9 @@ $(document).ready(function(){
       linkResources.removeClass("currentLink underLine");
 			linkContact.removeClass('currentLink underLine');
       homePage.stop().animate({"opacity": "1"}, 500);
+      homePage.css("opacity", "1");
   		downArr.stop().animate({"opacity": "1"}, 500);
+      downArr.css("opacity", "1");
 		}
 
     if(mission.offset().top < $(window).scrollTop() + $(window).outerHeight())
@@ -124,6 +126,24 @@ $(document).ready(function(){
       homePage.css("opacity", "0");
       downArr.stop().animate({"opacity": "0"}, 200);
       downArr.css("opacity", "0");
+    }
+
+    if ($(window).width() <= 641 && $(window).width() >= 320) {
+      annBTN.css("display", "none");
+      annBTN2.css("display", "block");
+
+      annBTN2.html('<a href="#news-events" class="moreannMobile">More Announcements</a>').appendTo(annBTN2);
+      //$("a.moreannMobile").css("cursor", "pointer");
+
+      annBTN2.click(function()
+      {
+        jQuery('html,body').stop().animate({scrollTop: newsEvents.offset().top}, 2000);
+
+        $(".article1Container > div:first").stop().fadeOut(200,
+        function(){
+          $(".article1Container > div:first").next().stop().fadeIn(1500).end().appendTo(".article1Container");
+        });
+      });
     }
 
     if(globalSec.offset().top < $(window).scrollTop() + $(window).outerHeight())
@@ -293,24 +313,6 @@ $(document).ready(function(){
         globalGraphic.stop().animate({"opacity": "1"}, 500);
       }
 
-      if ($(window).width() <= 641 && $(window).width() >= 320) {
-        annBTN.css("display", "none");
-        annBTN2.css("display", "block");
-
-        annBTN2.html('<a href="#news-events" class="moreannMobile">More Announcements</a>').appendTo(annBTN2);
-        //$("a.moreannMobile").css("cursor", "pointer");
-
-        annBTN2.click(function()
-      	{
-          jQuery('html,body').stop().animate({scrollTop: newsEvents.offset().top}, 2000);
-
-          $(".article1Container > div:first").stop().fadeOut(200,
-          function(){
-            $(".article1Container > div:first").next().stop().fadeIn(1500).end().appendTo(".article1Container");
-          });
-      	});
-      }
-
       if ($(window).width() <= 641 && $(window).width() >= 635) {
         globalSec.stop().animate({"padding-top": "43%"}, 500);
         globalIcon.stop().animate({"opacity": "1"}, 500);
@@ -389,7 +391,9 @@ $(document).ready(function(){
     linkResources.removeClass("currentLink underLine");
 		linkContact.removeClass("currentLink underLine");
     homePage.stop().animate({"opacity": "1"}, 500);
+    homePage.css("opacity", "1");
     downArr.stop().animate({"opacity": "1"}, 500);
+    downArr.css("opacity", "1");
 	});
 
   linkNews.click(function()
